0f9f9bbef277a7eaee7df25763e83f986565588e
Rodrigo Santiago <rodrigo@rocketcommerce.com.br>
2015-08-28 18:28:34 -0300

    refatoração e construção de atributos objetos

--- factory/builder/builder.go
+++ factory/builder/builder.go
@@ -31,24 +31,78 @@ func SetMap (t string, m map[string]string, context map[string]interface{}) {
 // 	return Map
 // }
 
-func buildMap (m map[string]string, c map[string]interface{}){
+func buildMap (m map[string]string, c map[string]interface{}) {
 
-	var buildedContext = make(map[string]interface{})
+	buildedContext := make(map[string]interface{})
 
 	for i, _ := range c {
+
+		var key string
+
+		key = m[i]
+
+		if isDotted(m[i]) {
+			key = strings.Split(m[i], ".")[0]
+		}
+
+		buildedContext[key] = reflect.ValueOf(parse(buildedContext[i], i, m[i], c[i])).Interface().(map[string]interface{})
+
+	}
+
+	fmt.Println("OPA", buildedContext)
+
+}
+
+func parse (buildedContext interface{}, i string, m string, c interface{}) interface{} {
+
+	if buildedContext == nil {
+
+		buildedContext := make(map[string]interface{})
+
+		externalKey := reflect.ValueOf(m).Interface().(string)
 
 		if isDotted(externalKey) {
 
 			s := strings.Split(externalKey, ".")
 
-			fmt.Println("composto", s[0])
+			//result := make(map[string]interface{})
+
+			for x, z := range s {
+				//primeiro infice não pega
+				if (x > 0) {
+
+					var currentIndexContextValue interface{}
+					var currentIndexMapValue interface{}
+
+					switch t := reflect.ValueOf(c).Interface().(type) {
+
+						case string, int64, float64:
+							currentIndexContextValue = t
+						default:
+							panic("Unknown value type")
+
+					}
+
+					currentIndexMapValue = reflect.ValueOf(m).Interface().(string)
+					
+					buildedContext = buildAttribute(buildedContext, z, currentIndexContextValue, currentIndexMapValue)
+
+					fmt.Println("123", buildedContext)
+				}
+
+			}
 
 		} else {
-			buildAttribute(buildedContext, i, c[i], m[i])
+			buildedContext = buildAttribute(buildedContext, i, c, m)
 		}
-	}
 
-	fmt.Println(buildedContext)
+		return buildedContext
+
+	} 
+
+
+	return buildedContext
+	
 
 }
 
@@ -57,11 +111,11 @@ func isDotted (value string) bool {
 	return match
 }
 
-func buildAttribute (container map[string]interface{}, key string, context interface{}, m interface{}) {
-
-	externalKey := reflect.ValueOf(m).Interface().(string)
+func buildAttribute (container map[string]interface{}, key string, context interface{}, m interface{}) map[string]interface{} {
 
 	t := reflect.TypeOf(context)
+
+	//k := reflect.ValueOf(m).Interface().(string)
 
 	if t.String() == "map[string]interface {}" || t.String() == "map[string]string" {
 
@@ -75,13 +129,19 @@ func buildAttribute (container map[string]interface{}, key string, context inter
 			result[i] = v
 		}
 
+		fmt.Println("TESTE", result)
+
 		container[key] = result
 
 
 	} else {
 
-		container[k] = context
+		fmt.Println(key)
+
+		container[key] = context
 	}
+
+	return container
 
 }
 
